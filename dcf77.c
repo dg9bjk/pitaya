e79086d2dcccc9100dbd2d0835c72fdc67fc0565
Jens <d9bjk@gmx.de>
2016-12-10 13:11:18 +0100

    modified:   .gitignore

--- dcf77.c
+++ dcf77.c
@@ -4,15 +4,34 @@
 #include <time.h>
 #include <rp.h>
 
+void sigcode(int Bitinfo)
+{
+      if(Bitinfo)
+      {	// long Pause
+        rp_GenAmp(RP_CH_1,0.2);
+        usleep(200000);
+        rp_GenAmp(RP_CH_1,1.0);
+        usleep(800000);
+      }
+      else
+      {
+        rp_GenAmp(RP_CH_1,0.2);
+        usleep(100000);
+        rp_GenAmp(RP_CH_1,1.0);
+        usleep(900000);
+      }
+}
 
 int main()
 {
-  int i,j;
-  int pos;
-  char display[101] = "";
+  int i,n;
+  int Bitinfo   = 0; 
+  int SwitchSec = 0;
+  time_t timestamp;
+  struct tm *ts;
   
   printf("\nDCF77-Tester \n");
-  printf("Attention: Sendung on 77,5kHz ! Only for Testing the Clock!\n");
+  printf("Attention: Sending on 77,5kHz ! Only for Testing the Clock!\n");
   
   if(rp_Init() != RP_OK)
   {
@@ -30,22 +49,33 @@ int main()
   rp_GenFreq(RP_CH_1,frequency1);
   rp_GenPhase(RP_CH_1,phase1);
   rp_GenAmp(RP_CH_1,amplitude1);
+
+  printf("Warte auf Minutenwechsel\n");  
+  do{
+    timestamp = time(NULL);
+    sleep(1);
+  }while((timestamp % 60) != 58);
+
   rp_GenOutEnable(RP_CH_1);
+  printf("Startup: 59 [Sync]\n");
+  sleep(1);
   
-  sleep(5);
-  
-  while(1)
+  n= 0;
+  while(n < 10)
   {
+    timestamp = time(NULL);
+    ts=localtime(&timestamp);
+    printf("Zeit: %02d.%02d.%04d - %02d:%02d:%02d \n",ts->tm_mday,ts->tm_mon+1,ts->tm_year+1900,ts->tm_hour,ts->tm_min,ts->tm_sec);
     for (i=0;i<59;i++)
     {
-    printf("Step: %d\n",i);
-    rp_GenAmp(RP_CH_1,0.2);
-    usleep(200000);
-    rp_GenAmp(RP_CH_1,1.0);
-    usleep(800000);
+      printf("Step: %d = %d\n",i,Bitinfo);
+      sigcode(Bitinfo);
     }
-    printf("LastStep: %d\n",i);
+    if(SwitchSec) 		// if we habe 61 Seconds (Switchseconds)
+      sigcode(Bitinfo);
+    printf("LastStep: %d [Sync]\n",i);
     usleep(1000000);
+    n++;
   }
 
   // Signalgenerator aus
